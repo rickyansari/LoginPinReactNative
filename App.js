53efd6062dadcc54a1ea1e48bad99c2a557ff1a9
ricky <ashfaq.m@techsophy.com>
2018-08-14 21:44:35 +0530

    added the deep linking for android

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import React, { Component } from 'react';
 import { ActivityIndicator, Dimensions, ListView, Image, Platform, Text, TouchableOpacity, View } 
 from 'react-native'; 
 import AppNavigator  from 'src/screens/AppNavigation'; 
+const prefix = Platform.OS == 'android' ? 'openMyApp://android/' : 'openMyApp://';
 
 export default class App extends Component<{}> { 
   render() { 
-    return (<AppNavigator />) 
+    return (<AppNavigator  uriPrefix={prefix}/>) 
   } 
 }
 
